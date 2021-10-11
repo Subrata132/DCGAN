07ef9a99649bb371d876d4164db32cd4067cbc2e
Subrata Biswas <subrata.biswwas@gmail.com>
2021-10-11 16:46:39 +0600

    Image data loader added.

---
+++ data_loader.py
@@ -0,0 +1,21 @@
+import cv2
+from os import listdir
+from torch.utils.data import Dataset
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_files = sorted(listdir(self.root_dir), key=len)
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        image = cv2.imread(self.root_dir+self.image_files[index], 0)
+        if self.transform:
+            return self.transform(image)
+        else:
+            return image
+
